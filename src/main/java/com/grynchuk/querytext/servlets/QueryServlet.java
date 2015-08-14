e3102eebf5232af76a685f0f0c557b7965ea5c9d
Uhbyxer <tgrynchuk@gmail.com>
2015-08-14 13:02:32 +0300

    1st commit

---
+++ src/main/java/com/grynchuk/querytext/servlets/QueryServlet.java
@@ -0,0 +1,26 @@
+package com.grynchuk.querytext.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class QueryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//throw new RuntimeException();
+		
+		response.getWriter().print("Hello");
+
+		//response.sendError(500);
+	}
+
+}
